0449861ea37115ae93db0efecc85b49cdc038456
Trần Minh Quang <tmq1512005@gmail.com>
2024-12-11 18:42:20 +0700

    mission

---
+++ PhanMemWebQuanLiBenhVien/PhanMemWebQuanLiBenhVien.DataAccess/Migrations/20241209165904_UpdateMissionpart2.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhanMemWebQuanLiBenhVien.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMissionpart2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_missions_doctors_DoctorId",
+                table: "missions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DoctorId",
+                table: "missions",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_missions_doctors_DoctorId",
+                table: "missions",
+                column: "DoctorId",
+                principalTable: "doctors",
+                principalColumn: "DoctorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_missions_doctors_DoctorId",
+                table: "missions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DoctorId",
+                table: "missions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_missions_doctors_DoctorId",
+                table: "missions",
+                column: "DoctorId",
+                principalTable: "doctors",
+                principalColumn: "DoctorId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- PhanMemWebQuanLiBenhVien/PhanMemWebQuanLiBenhVien.Models/Models/Mission.cs
+++ PhanMemWebQuanLiBenhVien/PhanMemWebQuanLiBenhVien.Models/Models/Mission.cs
@@ -10,11 +10,13 @@ namespace PhanMemWebQuanLiBenhVien.Models
         public int MissionId { get; set; }
         [Required(ErrorMessage = "Vui lòng nhập ngày bắt đầu.")]
         public DateTime Time { get; set; }
-
-        public ELever Lever { get; set; }
-
+        [Required(ErrorMessage ="Vui long nhập mức đọ. ")]
+        public Elever Lever { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập ngày kết thúc !")]
         public DateTime EndTime { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập nội dung !")]
         public string Content { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập ngày loại phòng !")]
         public EPhong RoomType { get; set; }
 
         public bool IsCompleted { get; set; } = false;
@@ -22,10 +24,10 @@ namespace PhanMemWebQuanLiBenhVien.Models
 
 
         //Foreign Keys
-        public int DoctorId { get; set; }
+        public int? DoctorId { get; set; }
 
         [ForeignKey("DoctorId")]
-        public Doctor Doctor { get; set; }
+        public Doctor? Doctor { get; set; }
 
         public int? PhongKhamId { get; set; }
 
