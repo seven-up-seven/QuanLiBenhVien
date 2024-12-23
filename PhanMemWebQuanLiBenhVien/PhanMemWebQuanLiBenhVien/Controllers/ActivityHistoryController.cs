37cbea9f6006ca4cb4c454d1d17acb9853662a00
nguyenminhquang <nnguyenminhquang786@gmail.com>
2024-12-23 21:09:48 +0700

    small updates

--- PhanMemWebQuanLiBenhVien/PhanMemWebQuanLiBenhVien/Controllers/ActivityHistoryController.cs
+++ PhanMemWebQuanLiBenhVien/PhanMemWebQuanLiBenhVien/Controllers/ActivityHistoryController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using DocumentFormat.OpenXml.EMMA;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using PhanMemWebQuanLiBenhVien.DataAccess.Repository.Interfaces;
 using PhanMemWebQuanLiBenhVien.Models;
+using static PhanMemWebQuanLiBenhVien.Ultilities.Utilities;
 
 namespace PhanMemWebQuanLiBenhVien.Controllers
 {
@@ -17,6 +20,39 @@ namespace PhanMemWebQuanLiBenhVien.Controllers
 
         public async Task<IActionResult> Index()
         {
+            ViewBag.ListRole = new SelectList(new List<SelectListItem>
+            {
+                new SelectListItem
+                {
+                    Text = "Quản lí nhân sự",
+                    Value = ERole.quanlinhansu.ToString()
+                },
+                new SelectListItem
+                {
+                    Text = "Quản lí vật tư",
+                    Value = ERole.quanlivattu.ToString()
+                },
+                new SelectListItem
+                {
+                    Text = "Quản lí bệnh nhân",
+                    Value = ERole.quanlibenhnhan.ToString()
+                },
+                new SelectListItem
+                {
+                    Text="Bác sĩ",
+                    Value=ERole.doctor.ToString()
+                },
+                new SelectListItem
+                {
+                    Text="Y tá",
+                    Value=ERole.nurse.ToString()
+                },
+                new SelectListItem
+                {
+                    Text="Admin",
+                    Value=ERole.admin.ToString()
+                }
+            }, "Value", "Text");
             var listactivity=_unitOfWork.ActivityHistoryRepository.GetAll();
             if (User.IsInRole("Doctor"))
             {
@@ -43,6 +79,77 @@ namespace PhanMemWebQuanLiBenhVien.Controllers
                 listactivity = listactivity.Where(u => u.MemberRole == Ultilities.Utilities.ERole.quanlibenhnhan && u.MemberId == true_user.UserId);
             }
             return View(listactivity.OrderByDescending(u=>u.ActivityTime).ToList());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(string SearchRole, int SearchExecutorID)
+        {
+            ViewBag.ListRole = new SelectList(new List<SelectListItem>
+            {
+                new SelectListItem
+                {
+                    Text = "Quản lí nhân sự",
+                    Value = ERole.quanlinhansu.ToString()
+                },
+                new SelectListItem
+                {
+                    Text = "Quản lí vật tư",
+                    Value = ERole.quanlivattu.ToString()
+                },
+                new SelectListItem
+                {
+                    Text = "Quản lí bệnh nhân",
+                    Value = ERole.quanlibenhnhan.ToString()
+                },
+                new SelectListItem
+                {
+                    Text="Bác sĩ",
+                    Value=ERole.doctor.ToString()
+                },
+                new SelectListItem
+                {
+                    Text="Y tá",
+                    Value=ERole.nurse.ToString()
+                },
+                new SelectListItem
+                {
+                    Text="Admin",
+                    Value=ERole.admin.ToString()
+                }
+            }, "Value", "Text");
+            var listactivity = _unitOfWork.ActivityHistoryRepository.GetAll();
+            if (User.IsInRole("Doctor"))
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var true_user = (CustomedUser)user;
+                listactivity = listactivity.Where(u => u.MemberRole == Ultilities.Utilities.ERole.doctor && u.MemberId == true_user.UserId);
+            }
+            if (User.IsInRole("Nurse"))
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var true_user = (CustomedUser)user;
+                listactivity = listactivity.Where(u => u.MemberRole == Ultilities.Utilities.ERole.nurse && u.MemberId == true_user.UserId);
+            }
+            if (User.IsInRole("QuanLiVatTu"))
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var true_user = (CustomedUser)user;
+                listactivity = listactivity.Where(u => u.MemberRole == Ultilities.Utilities.ERole.quanlivattu && u.MemberId == true_user.UserId);
+            }
+            if (User.IsInRole("QuanLiBenhNhan"))
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var true_user = (CustomedUser)user;
+                listactivity = listactivity.Where(u => u.MemberRole == Ultilities.Utilities.ERole.quanlibenhnhan && u.MemberId == true_user.UserId);
+            }
+            if (!string.IsNullOrEmpty(SearchExecutorID.ToString()))
+            {
+                listactivity = listactivity.Where(u => u.MemberId == SearchExecutorID);
+            }
+            if (SearchRole!="NoFilter")
+            {
+                listactivity = listactivity.Where(u => u.MemberRole.ToString() == SearchRole);
+            }
+            return View(listactivity.OrderByDescending(u => u.ActivityTime).ToList());
         }
     }
 }
