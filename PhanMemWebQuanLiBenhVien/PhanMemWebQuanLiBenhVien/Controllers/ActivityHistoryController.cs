cd6fe9d9748fbc453444cfac9240e09d91d135f1
nguyenminhquang <nnguyenminhquang786@gmail.com>
2024-12-23 22:33:02 +0700

    updates

--- PhanMemWebQuanLiBenhVien/PhanMemWebQuanLiBenhVien/Controllers/ActivityHistoryController.cs
+++ PhanMemWebQuanLiBenhVien/PhanMemWebQuanLiBenhVien/Controllers/ActivityHistoryController.cs
@@ -18,39 +18,39 @@ namespace PhanMemWebQuanLiBenhVien.Controllers
             _userManager = userManager;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? ExecutorID=null)
         {
             ViewBag.ListRole = new SelectList(new List<SelectListItem>
             {
                 new SelectListItem
                 {
                     Text = "Quản lí nhân sự",
-                    Value = ERole.quanlinhansu.ToString()
+                    Value = ERole.quanlinhansu.ToString(),
                 },
                 new SelectListItem
                 {
                     Text = "Quản lí vật tư",
-                    Value = ERole.quanlivattu.ToString()
+                    Value = ERole.quanlivattu.ToString(),
                 },
                 new SelectListItem
                 {
                     Text = "Quản lí bệnh nhân",
-                    Value = ERole.quanlibenhnhan.ToString()
+                    Value = ERole.quanlibenhnhan.ToString(),
                 },
                 new SelectListItem
                 {
                     Text="Bác sĩ",
-                    Value=ERole.doctor.ToString()
+                    Value=ERole.doctor.ToString(),
                 },
                 new SelectListItem
                 {
                     Text="Y tá",
-                    Value=ERole.nurse.ToString()
+                    Value=ERole.nurse.ToString(),
                 },
                 new SelectListItem
                 {
                     Text="Admin",
-                    Value=ERole.admin.ToString()
+                    Value=ERole.admin.ToString(),
                 }
             }, "Value", "Text");
             var listactivity=_unitOfWork.ActivityHistoryRepository.GetAll();
@@ -78,6 +78,10 @@ namespace PhanMemWebQuanLiBenhVien.Controllers
                 var true_user = (CustomedUser)user;
                 listactivity = listactivity.Where(u => u.MemberRole == Ultilities.Utilities.ERole.quanlibenhnhan && u.MemberId == true_user.UserId);
             }
+            if (ExecutorID!=null)
+            {
+                listactivity=listactivity.Where(u=>u.MemberId==ExecutorID);
+            }
             return View(listactivity.OrderByDescending(u=>u.ActivityTime).ToList());
         }
         [HttpPost]
@@ -88,36 +92,36 @@ namespace PhanMemWebQuanLiBenhVien.Controllers
                 new SelectListItem
                 {
                     Text = "Quản lí nhân sự",
-                    Value = ERole.quanlinhansu.ToString()
+                    Value = ERole.quanlinhansu.ToString(),
                 },
                 new SelectListItem
                 {
                     Text = "Quản lí vật tư",
-                    Value = ERole.quanlivattu.ToString()
+                    Value = ERole.quanlivattu.ToString(),
                 },
                 new SelectListItem
                 {
                     Text = "Quản lí bệnh nhân",
-                    Value = ERole.quanlibenhnhan.ToString()
+                    Value = ERole.quanlibenhnhan.ToString(),
                 },
                 new SelectListItem
                 {
                     Text="Bác sĩ",
-                    Value=ERole.doctor.ToString()
+                    Value=ERole.doctor.ToString(),
                 },
                 new SelectListItem
                 {
                     Text="Y tá",
-                    Value=ERole.nurse.ToString()
+                    Value=ERole.nurse.ToString(),
                 },
                 new SelectListItem
                 {
                     Text="Admin",
-                    Value=ERole.admin.ToString()
+                    Value=ERole.admin.ToString(),
                 }
             }, "Value", "Text");
             var listactivity = _unitOfWork.ActivityHistoryRepository.GetAll();
-            if (User.IsInRole("Doctor"))
+            /*if (User.IsInRole("Doctor"))
             {
                 var user = await _userManager.GetUserAsync(User);
                 var true_user = (CustomedUser)user;
@@ -140,8 +144,8 @@ namespace PhanMemWebQuanLiBenhVien.Controllers
                 var user = await _userManager.GetUserAsync(User);
                 var true_user = (CustomedUser)user;
                 listactivity = listactivity.Where(u => u.MemberRole == Ultilities.Utilities.ERole.quanlibenhnhan && u.MemberId == true_user.UserId);
-            }
-            if (!string.IsNullOrEmpty(SearchExecutorID.ToString()))
+            }*/
+            if (SearchExecutorID!=-1)
             {
                 listactivity = listactivity.Where(u => u.MemberId == SearchExecutorID);
             }
